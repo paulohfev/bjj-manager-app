6cdf745ad5a045e89c385b3594b90c80ddc2e329
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2024-07-23 22:50:50 -0300

    Add month navigation buttons

--- src/app/_presenter/components/Calendar/styles.ts
+++ src/app/_presenter/components/Calendar/styles.ts
@@ -1,4 +1,11 @@
 const styles = {
+  arrowButton: {
+    height: '24px',
+    minWidth: 'fit-content',
+    padding: 0,
+    width: '24px',
+  },
+
   calendarDay: {
     cursor: 'pointer',
     height: '52px',
@@ -6,14 +13,27 @@ const styles = {
     width: '52px',
   },
 
-  monthTitle: {
-    margin: '0 0 60px',
+  currentDateButton: {
+    margin: '0 6px 0 3px',
+  },
+
+  dateHeaderContainer: {
+    textAlign: 'center',
+  },
+
+  navigationContainer: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column' as const,
+    gap: '10px',
+    padding: '20px 0',
   },
 
   selectedCalendarDay: (isDaySelected: boolean) => ({
     alignItems: 'center',
     background: isDaySelected ? 'linear-gradient(90deg, #FF465D 0%, #BC46BA 100%)' : 'transparent',
     borderRadius: '50%',
+    color: isDaySelected ? '#fff' : '#000',
     display: 'flex',
     height: '36px',
     justifyContent: 'center',
@@ -26,10 +46,6 @@ const styles = {
     padding: '0 15px 15px',
     textAlign: 'center',
     textTransform: 'uppercase',
-  },
-
-  yearTitle: {
-    margin: 0,
   },
 }
 
