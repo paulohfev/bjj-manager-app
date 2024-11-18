8f2e5fae89bc257f9aa095747edf99fd09e30435
paulohfev <phfernandesev@gmail.com>
2024-11-17 22:40:41 -0300

    Add authorization and audit authentication

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,10 @@
-import NextAuth from 'next-auth'
+import NextAuth, { Session, User } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
 import { APP_ROUTES } from '@/constants/appRoutes'
 import { login } from '@/services/LoginService'
 import { Login } from '@/types/api/Login'
+import { AuthSession, AuthToken, AuthUser } from '@/types/CustomAuth'
 
 const authOptions = {
   providers: [
@@ -35,6 +36,7 @@ const authOptions = {
         const user = {
           id: response.userId,
           email: response.email,
+          token: response.token,
         }
 
         return user
@@ -44,6 +46,30 @@ const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: APP_ROUTES.login,
+  },
+  callbacks: {
+    async jwt({ token, user }: { token: AuthToken; user?: User }) {
+      // On sign-in, add user ID to the token to be used in the session method
+      if (user) {
+        token.userId = user.id
+        token.accessToken = (user as AuthUser).token
+      }
+      return token
+    },
+    async session({ session, token }: { session: Session; token: AuthToken }) {
+      // Add user ID from token, passed from the jwt method, to the session object
+      // This is necessary to make the session object available in the client via useSession()
+      const customSession: AuthSession = {
+        ...session,
+        accessToken: token.accessToken,
+        user: {
+          id: token.userId as string,
+          ...session.user,
+        },
+      }
+
+      return customSession
+    },
   },
 }
 
